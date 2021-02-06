399bb7c31f2234a3fd921d9ed02848a5968ec7a0
Emil <emilpet93@gmail.com>
2021-02-06 18:46:07 +0100

    Create an apollo client and remove strapi fetchApi

---
+++ frontend/api/apollo.js
@@ -0,0 +1,13 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { withApollo } from 'next-apollo'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: `${API_URL}/graphql`,
+  }),
+  cache: new InMemoryCache(),
+})
+
+export default withApollo(apolloClient)
