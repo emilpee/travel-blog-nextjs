7fddf84f512c61c27c7911e7e5f1b33d9b8d0d68
Emil <emilpet93@gmail.com>
2021-04-11 15:20:38 +0200

    Use small screen query in navbar

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import { Box, Heading, Flex, Text } from '@chakra-ui/react'
+import { Box, Heading, Flex, Text, useMediaQuery } from '@chakra-ui/react'
 import Link from 'next/link'
 import { gql, useQuery } from '@apollo/client'
+import { useEffect, useState } from 'react'
 
 const QUERY = gql`
   {
@@ -14,13 +15,21 @@ const QUERY = gql`
 
 const Navbar = () => {
   const { loading, error, data } = useQuery(QUERY)
+  const [isMobile, setIsMobile] = useState(false)
+  const smallScreen = useMediaQuery('(max-width: 768px)')
+
+  useEffect(() => {
+    if (smallScreen !== isMobile) {
+      setIsMobile(smallScreen[0])
+    }
+  }, [isMobile, smallScreen])
 
   if (error) return 'Error loading categories'
   if (loading) return <h2>Fetching...</h2>
 
   return (
     <Box bg="orange.300" padding="1" color="white">
-      <Flex direction="row">
+      <Flex alignItems={isMobile ? 'center' : 'flex-start'} direction={isMobile ? 'column' : 'row'}>
         <Heading padding="4" as="h3" size="xl">
           <Link href="/">Travel blog</Link>
         </Heading>
